789587bbd03f3f07dffe7c6e46221db44bd79037
dayumstir <dempstertay123@gmail.com>
2025-01-22 23:07:18 +0800

    fix: auth redirect

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,6 +9,9 @@ export default function Auth() {
   const handleGoogleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
     });
   };
 
