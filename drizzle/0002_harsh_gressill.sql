34c3009077b236917e7171df287847496db71acb
dayumstir <dempstertay123@gmail.com>
2025-01-22 22:34:13 +0800

    feat: update schema

---
+++ drizzle/0002_harsh_gressill.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS "user_to_group" (
+	"user_id" uuid NOT NULL,
+	"group_id" uuid NOT NULL,
+	CONSTRAINT "user_to_group_user_id_group_id_pk" PRIMARY KEY("user_id","group_id")
+);
+--> statement-breakpoint
+DROP TABLE "user_group";--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "user_to_group" ADD CONSTRAINT "user_to_group_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "user_to_group" ADD CONSTRAINT "user_to_group_group_id_group_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."group"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,6 @@
 import { relations } from "drizzle-orm";
 import {
-  pgTable as table,
+  pgTable,
   varchar,
   timestamp,
   numeric,
@@ -9,49 +9,20 @@ import {
 } from "drizzle-orm/pg-core";
 
 // User
-export const user = table("user", {
+export const user = pgTable("user", {
   id: uuid("id").primaryKey(),
   name: varchar("name").notNull(),
   email: varchar("email").notNull().unique(),
   avatarUrl: varchar("avatar_url"),
   createdAt: timestamp("created_at").defaultNow(),
 });
-
 export const userRelations = relations(user, ({ many }) => ({
-  groups: many(group),
   expenses: many(expense),
+  groups: many(userToGroup),
 }));
 
-// Group
-export const group = table("group", {
-  id: uuid("id").primaryKey(),
-  name: varchar("name").notNull(),
-  currency: varchar("currency").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-  categories: varchar("categories").array(),
-  creatorId: uuid("creator_id")
-    .references(() => user.id)
-    .notNull(),
-});
-
-// User Group (Junction table for many-to-many relationship)
-export const userGroup = table(
-  "user_group",
-  {
-    userId: uuid("user_id")
-      .references(() => user.id)
-      .notNull(),
-    groupId: uuid("group_id")
-      .references(() => group.id)
-      .notNull(),
-  },
-  (table) => ({
-    pk: primaryKey({ columns: [table.userId, table.groupId] }),
-  })
-);
-
 // Expense
-export const expense = table("expense", {
+export const expense = pgTable("expense", {
   id: uuid("id").primaryKey(),
   title: varchar("title").notNull(),
   amount: numeric("amount", { precision: 10, scale: 2 }).notNull(),
@@ -64,6 +35,62 @@ export const expense = table("expense", {
     .references(() => group.id)
     .notNull(),
 });
+export const expenseRelations = relations(expense, ({ one }) => ({
+  paidBy: one(user, {
+    fields: [expense.paidBy],
+    references: [user.id],
+  }),
+  groupId: one(group, {
+    fields: [expense.groupId],
+    references: [group.id],
+  }),
+}));
+
+// Group
+export const group = pgTable("group", {
+  id: uuid("id").primaryKey(),
+  name: varchar("name").notNull(),
+  currency: varchar("currency").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  categories: varchar("categories").array(),
+  creatorId: uuid("creator_id")
+    .references(() => user.id)
+    .notNull(),
+});
+export const groupRelations = relations(group, ({ one, many }) => ({
+  creator: one(user, {
+    fields: [group.creatorId],
+    references: [user.id],
+  }),
+  expenses: many(expense),
+  members: many(userToGroup),
+}));
+
+// User to Group (Many to Many)
+export const userToGroup = pgTable(
+  "user_to_group",
+  {
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => user.id),
+    groupId: uuid("group_id")
+      .notNull()
+      .references(() => group.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.groupId] }),
+  }),
+);
+export const userToGroupRelations = relations(userToGroup, ({ one }) => ({
+  user: one(user, {
+    fields: [userToGroup.userId],
+    references: [user.id],
+  }),
+  group: one(group, {
+    fields: [userToGroup.groupId],
+    references: [group.id],
+  }),
+}));
 
 // Expense Participants
 // export const expenseParticipants = table(
